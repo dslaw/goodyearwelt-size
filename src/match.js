e1daa4b14ced162cba2a034424c142e2b1b71d86
David Law <davidsamuellaw@gmail.com>
2018-07-06 15:10:57 -0700

    Style changes

--- src/match.js
+++ src/match.js
@@ -1,26 +1,26 @@
-const match_regex = function(line, pattern) {
-  let ret = pattern.exec(line);
+const matchRegex = function(line, pattern) {
+  const ret = pattern.exec(line);
   if (!ret) {
     // Return empty object instead of null for
     // destructuring.
     return {};
   }
 
-  let [match, ...captures] = ret;
-  let {index} = ret;
-  return {match, captures, index};
+  const [ match, ...captures ] = ret;
+  const { index } = ret;
+  return { match, captures, index };
 };
 
-const match_size = function(line) {
+const matchSize = function(line) {
   // TODO: Will match a whole size if first decimal is not a
   //       half size, i.e. 8.2 => 8.
   //       If someone meant a half size, but made a typo, the
   //       whole size will be returned!
-  let pattern = /[0-9]{1,2}(\.[05])?/;
-  return match_regex(line, pattern);
+  const pattern = /[0-9]{1,2}(\.[05])?/;
+  return matchRegex(line, pattern);
 };
 
-const match_width = function(line) {
+const matchWidth = function(line) {
   // The pattern /E(?!U)/ stops the regex from matching
   // the 'E' in 'EU' when no width is specified. But if
   // there is no space between an 'E' width and an intl
@@ -28,41 +28,41 @@ const match_width = function(line) {
   // case can be matched via /E(?=U)/, but then the former
   // will be broken. We opt to handle the former as it is
   // significantly more common.
-  let pattern = /^\s?((EEE)|(EE)|(E(?!U))|[ABCDFG])/i;
-  return match_regex(line, pattern);
+  const pattern = /^\s?((EEE)|(EE)|(E(?!U))|[ABCDFG])/i;
+  return matchRegex(line, pattern);
 };
 
-const match_intl = function(line) {
-  let pattern = /^\s?(US|UK|EU)/i;
-  return match_regex(line, pattern);
+const matchIntl = function(line) {
+  const pattern = /^\s?(US|UK|EU)/i;
+  return matchRegex(line, pattern);
 };
 
-const match_intl_preceding = (line) => {
-  let pattern = /(US|UK|(EU)R?)(?=\s?[0-9])/i;
-  return match_regex(line, pattern);
+const matchIntlPreceding = function(line) {
+  const pattern = /(US|UK|(EU)R?)(?=\s?[0-9])/i;
+  return matchRegex(line, pattern);
 };
 
-const collapse_spaces = function(string) {
+const collapseSpaces = function(string) {
   return string.replace(/\s+/g, ' ');
 };
 
-const sticky_match = function(line, fns, n_chars) {
+const stickyMatch = function(line, fns, nChars) {
   // Ensure output is always uppercase. Spacing won't be returned.
-  let input_string = collapse_spaces(line).toUpperCase();
-  let matches = [];
-  let start_pos = 0
-    end_pos = input_string.length;
+  const inputString = collapseSpaces(line).toUpperCase();
+  const matches = [];
+  let startPos = 0
+    endPos = inputString.length;
 
-  for (let fn of fns) {
-    let string = input_string.slice(start_pos, end_pos);
-    let {match, captures, index} = fn(string);
+  for (const fn of fns) {
+    const string = inputString.slice(startPos, endPos);
+    let { match, captures, index } = fn(string);
     if (match) {
       // Constrain the search space after the number is found,
       // as the pattern for `width` may generate a lot of false
       // positives, otherwise (i.e. if notes are present and not
       // international convention).
-      start_pos += index + match.length;
-      end_pos = start_pos + n_chars;
+      startPos += index + match.length;
+      endPos = startPos + nChars;
       match = match.trim();
     }
     matches.push(match || null);
@@ -71,10 +71,10 @@ const sticky_match = function(line, fns, n_chars) {
   return matches;
 };
 
-const post_match = function(line) {
-  let fns = [match_size, match_width, match_intl];
-  let max_chars_ahead = 4 // Space + three chars for 'EEE'.
-  let matches = sticky_match(line, fns, max_chars_ahead);
+const postMatch = function(line) {
+  const fns = [ matchSize, matchWidth, matchIntl ];
+  const maxCharsAhead = 4;  // Space + three chars for 'EEE'.
+  const matches = stickyMatch(line, fns, maxCharsAhead);
   return {
     size: matches[0],
     width: matches[1],
@@ -82,10 +82,10 @@ const post_match = function(line) {
   };
 };
 
-const precedes_match = function(line) {
-  let fns = [match_intl_preceding, match_size, match_width];
-  let max_chars_ahead = 6 // Space, R (from EUR) and four chars for e.g. 11.5.
-  let matches = sticky_match(line, fns, max_chars_ahead);
+const precedesMatch = function(line) {
+  const fns = [ matchIntlPreceding, matchSize, matchWidth ];
+  const maxCharsAhead = 6; // Space, R (from EUR) and four chars for e.g. 11.5.
+  const matches = stickyMatch(line, fns, maxCharsAhead);
   return {
     size: matches[1],
     width: matches[2],
@@ -95,6 +95,6 @@ const precedes_match = function(line) {
 
 
 module.exports = {
-  post_match: post_match,
-  precedes_match: precedes_match,
+  postMatch,
+  precedesMatch,
 };
--- test/test_match.js
+++ test/test_match.js
@@ -3,8 +3,8 @@ const rewire = require('rewire');
 const match = rewire('../src/match.js');
 
 // Private functions.
-const collapse_spaces = match.__get__('collapse_spaces');
-const match_regex = match.__get__('match_regex');
+const collapseSpaces = match.__get__('collapseSpaces');
+const matchRegex = match.__get__('matchRegex');
 
 
 describe('Match regular expression', () => {
@@ -13,17 +13,17 @@ describe('Match regular expression', () => {
   it('Should match', () => {
     const pattern = /cd/;
 
-    const {match, captures, index} = match_regex(line, pattern);
+    const { match, captures, index } = matchRegex(line, pattern);
     assert.strictEqual(match, 'cd');
     assert.strictEqual(captures.length, 0);
     assert.strictEqual(index, 2);
   });
 
   it('Should destructure to undefined', () => {
-    const {match, captures, index} = match_regex(line, /nomatch/);
-    assert.ok(match === undefined);
-    assert.ok(captures === undefined);
-    assert.ok(index === undefined);
+    const { match, captures, index } = matchRegex(line, /nomatch/);
+    assert.strictEqual(match, undefined);
+    assert.strictEqual(captures, undefined);
+    assert.strictEqual(index, undefined);
   });
 });
 
@@ -38,7 +38,7 @@ describe('Collapse whitespace to a single space', () => {
 
   inputs.forEach((input) => {
     it(`Should collapse whitespace for ${input}`, () => {
-      const out = collapse_spaces(input);
+      const out = collapseSpaces(input);
       assert.strictEqual(out, expected);
     });
   });
@@ -46,188 +46,188 @@ describe('Collapse whitespace to a single space', () => {
 
 describe('Match specific patterns', () => {
   const notes = ', words (and another 9UK size).';
-  const input_expectations = [
+  const inputExpectations = [
     // Whole sizes, no international specification.
-    {line: '8', size: '8', width: null, intl: null},
-    {line: '8D', size: '8', width: 'D', intl: null},
+    { line: '8', size: '8', width: null, intl: null },
+    { line: '8D', size: '8', width: 'D', intl: null },
 
     // Double digit sizes, no international specification.
-    {line: '11', size: '11', width: null, intl: null},
-    {line: '11D', size: '11', width: 'D', intl: null},
+    { line: '11', size: '11', width: null, intl: null },
+    { line: '11D', size: '11', width: 'D', intl: null },
 
     // Half sizes, no international specification.
-    {line: '8.5', size: '8.5', width: null, intl: null},
-    {line: '8.5D', size: '8.5', width: 'D', intl: null},
+    { line: '8.5', size: '8.5', width: null, intl: null },
+    { line: '8.5D', size: '8.5', width: 'D', intl: null },
 
     // Double digit half sizes, no international specification.
-    {line: '11.5', size: '11.5', width: null, intl: null},
-    {line: '11.5D', size: '11.5', width: 'D', intl: null},
+    { line: '11.5', size: '11.5', width: null, intl: null },
+    { line: '11.5D', size: '11.5', width: 'D', intl: null },
 
     // Whole sizes, US.
-    {line: '8D US', size: '8', width: 'D', intl: 'US'},
-    {line: '8DUS', size: '8', width: 'D', intl: 'US'},
-    {line: '8 US', size: '8', width: null, intl: 'US'},
-    {line: '8US', size: '8', width: null, intl: 'US'},
-    {line: '8.0 US', size: '8.0', width: null, intl: 'US'},
-    {line: '8.0US', size: '8.0', width: null, intl: 'US'},
+    { line: '8D US', size: '8', width: 'D', intl: 'US' },
+    { line: '8DUS', size: '8', width: 'D', intl: 'US' },
+    { line: '8 US', size: '8', width: null, intl: 'US' },
+    { line: '8US', size: '8', width: null, intl: 'US' },
+    { line: '8.0 US', size: '8.0', width: null, intl: 'US' },
+    { line: '8.0US', size: '8.0', width: null, intl: 'US' },
 
     // Double digit whole sizes, US.
-    {line: '11D US', size: '11', width: 'D', intl: 'US'},
-    {line: '11DUS', size: '11', width: 'D', intl: 'US'},
-    {line: '11 US', size: '11', width: null, intl: 'US'},
-    {line: '11US', size: '11', width: null, intl: 'US'},
-    {line: '11.0 US', size: '11.0', width: null, intl: 'US'},
-    {line: '11.0US', size: '11.0', width: null, intl: 'US'},
+    { line: '11D US', size: '11', width: 'D', intl: 'US' },
+    { line: '11DUS', size: '11', width: 'D', intl: 'US' },
+    { line: '11 US', size: '11', width: null, intl: 'US' },
+    { line: '11US', size: '11', width: null, intl: 'US' },
+    { line: '11.0 US', size: '11.0', width: null, intl: 'US' },
+    { line: '11.0US', size: '11.0', width: null, intl: 'US' },
 
     // Half sizes, US.
-    {line: '8.5 US', size: '8.5', width: null, intl: 'US'},
-    {line: '8.5US', size: '8.5', width: null, intl: 'US'},
-    {line: '8.5D US', size: '8.5', width: 'D', intl: 'US'},
-    {line: '8.5DUS', size: '8.5', width: 'D', intl: 'US'},
+    { line: '8.5 US', size: '8.5', width: null, intl: 'US' },
+    { line: '8.5US', size: '8.5', width: null, intl: 'US' },
+    { line: '8.5D US', size: '8.5', width: 'D', intl: 'US' },
+    { line: '8.5DUS', size: '8.5', width: 'D', intl: 'US' },
 
     // Double digit half sizes, US.
-    {line: '11.5 US', size: '11.5', width: null, intl: 'US'},
-    {line: '11.5US', size: '11.5', width: null, intl: 'US'},
-    {line: '11.5D US', size: '11.5', width: 'D', intl: 'US'},
-    {line: '11.5DUS', size: '11.5', width: 'D', intl: 'US'},
+    { line: '11.5 US', size: '11.5', width: null, intl: 'US' },
+    { line: '11.5US', size: '11.5', width: null, intl: 'US' },
+    { line: '11.5D US', size: '11.5', width: 'D', intl: 'US' },
+    { line: '11.5DUS', size: '11.5', width: 'D', intl: 'US' },
 
     // Alternate widthes.
-    {line: '8A US', size: '8', 'width': 'A', intl: 'US'},
-    {line: '8B US', size: '8', 'width': 'B', intl: 'US'},
-    {line: '8C US', size: '8', 'width': 'C', intl: 'US'},
-    {line: '8E US', size: '8', 'width': 'E', intl: 'US'},
-    {line: '8EE US', size: '8', 'width': 'EE', intl: 'US'},
-    {line: '8EEE US', size: '8', 'width': 'EEE', intl: 'US'},
-    {line: '8F US', size: '8', 'width': 'F', intl: 'US'},
-    {line: '8G US', size: '8', 'width': 'G', intl: 'US'},
+    { line: '8A US', size: '8', 'width': 'A', intl: 'US' },
+    { line: '8B US', size: '8', 'width': 'B', intl: 'US' },
+    { line: '8C US', size: '8', 'width': 'C', intl: 'US' },
+    { line: '8E US', size: '8', 'width': 'E', intl: 'US' },
+    { line: '8EE US', size: '8', 'width': 'EE', intl: 'US' },
+    { line: '8EEE US', size: '8', 'width': 'EEE', intl: 'US' },
+    { line: '8F US', size: '8', 'width': 'F', intl: 'US' },
+    { line: '8G US', size: '8', 'width': 'G', intl: 'US' },
 
     // UK.
-    {line: '8D UK', size: '8', width: 'D', intl: 'UK'},
-    {line: '8DUK', size: '8', width: 'D', intl: 'UK'},
-    {line: '8 UK', size: '8', width: null, intl: 'UK'},
-    {line: '8UK', size: '8', width: null, intl: 'UK'},
-    {line: '8.0 UK', size: '8.0', width: null, intl: 'UK'},
-    {line: '8.0UK', size: '8.0', width: null, intl: 'UK'},
+    { line: '8D UK', size: '8', width: 'D', intl: 'UK' },
+    { line: '8DUK', size: '8', width: 'D', intl: 'UK' },
+    { line: '8 UK', size: '8', width: null, intl: 'UK' },
+    { line: '8UK', size: '8', width: null, intl: 'UK' },
+    { line: '8.0 UK', size: '8.0', width: null, intl: 'UK' },
+    { line: '8.0UK', size: '8.0', width: null, intl: 'UK' },
 
     // EU.
-    {line: '44D EU', size: '44', width: 'D', intl: 'EU'},
-    {line: '44DEU', size: '44', width: 'D', intl: 'EU'},
-    {line: '44 EU', size: '44', width: null, intl: 'EU'},
-    {line: '44EU', size: '44', width: null, intl: 'EU'},
-    {line: '44.0 EU', size: '44.0', width: null, intl: 'EU'},
-    {line: '44.0EU', size: '44.0', width: null, intl: 'EU'},
+    { line: '44D EU', size: '44', width: 'D', intl: 'EU' },
+    { line: '44DEU', size: '44', width: 'D', intl: 'EU' },
+    { line: '44 EU', size: '44', width: null, intl: 'EU' },
+    { line: '44EU', size: '44', width: null, intl: 'EU' },
+    { line: '44.0 EU', size: '44.0', width: null, intl: 'EU' },
+    { line: '44.0EU', size: '44.0', width: null, intl: 'EU' },
 
     // EUR.
-    {line: '44D EUR', size: '44', width: 'D', intl: 'EU'},
-    {line: '44DEUR', size: '44', width: 'D', intl: 'EU'},
-    {line: '44 EUR', size: '44', width: null, intl: 'EU'},
-    {line: '44EUR', size: '44', width: null, intl: 'EU'},
-    {line: '44.0 EUR', size: '44.0', width: null, intl: 'EU'},
-    {line: '44.0EUR', size: '44.0', width: null, intl: 'EU'},
+    { line: '44D EUR', size: '44', width: 'D', intl: 'EU' },
+    { line: '44DEUR', size: '44', width: 'D', intl: 'EU' },
+    { line: '44 EUR', size: '44', width: null, intl: 'EU' },
+    { line: '44EUR', size: '44', width: null, intl: 'EU' },
+    { line: '44.0 EUR', size: '44.0', width: null, intl: 'EU' },
+    { line: '44.0EUR', size: '44.0', width: null, intl: 'EU' },
 
     // Misc edge cases.
-    {line: '10.5E,', size: '10.5', width: 'E', intl: null},
-    {line: '8 (best...)', size: '8', width: null, intl: null},
-    {line: '8/8E', size: '8', width: null, intl: null},
+    { line: '10.5E,', size: '10.5', width: 'E', intl: null },
+    { line: '8 (best...)', size: '8', width: null, intl: null },
+    { line: '8/8E', size: '8', width: null, intl: null },
   ];
 
   // Parametrize.
-  input_expectations.forEach((obj) => {
+  inputExpectations.forEach(obj => {
     it(`Should extract ordered data from '${obj.line}'`, () => {
       const expected = {
         size: obj.size,
         width: obj.width,
         intl: obj.intl,
       };
-      const out = match.post_match(obj.line);
+      const out = match.postMatch(obj.line);
       assert.deepStrictEqual(out, expected);
     });
   });
 
-  input_expectations.forEach((obj) => {
+  inputExpectations.forEach(obj => {
     it(`Should extract ordered data when addtl notes are present from '${obj.line}'`, () => {
       const expected = {
         size: obj.size,
         width: obj.width,
         intl: obj.intl,
       };
-      const out = match.post_match(obj.line + notes);
+      const out = match.postMatch(obj.line + notes);
       assert.deepStrictEqual(out, expected);
     });
   });
 
-  const input_expectations_precedes = [
+  const inputExpectationsPrecedes = [
     // US precedes size.
-    {line: 'US8', size: '8', width: null, intl: 'US'},
-    {line: 'US 8', size: '8', width: null, intl: 'US'},
-    {line: 'US8D', size: '8', width: 'D', intl: 'US'},
-    {line: 'US 8D', size: '8', width: 'D', intl: 'US'},
-    {line: 'US8.5', size: '8.5', width: null, intl: 'US'},
-    {line: 'US 8.5', size: '8.5', width: null, intl: 'US'},
-    {line: 'US8.5D', size: '8.5', width: 'D', intl: 'US'},
-    {line: 'US 8.5D', size: '8.5', width: 'D', intl: 'US'},
+    { line: 'US8', size: '8', width: null, intl: 'US' },
+    { line: 'US 8', size: '8', width: null, intl: 'US' },
+    { line: 'US8D', size: '8', width: 'D', intl: 'US' },
+    { line: 'US 8D', size: '8', width: 'D', intl: 'US' },
+    { line: 'US8.5', size: '8.5', width: null, intl: 'US' },
+    { line: 'US 8.5', size: '8.5', width: null, intl: 'US' },
+    { line: 'US8.5D', size: '8.5', width: 'D', intl: 'US' },
+    { line: 'US 8.5D', size: '8.5', width: 'D', intl: 'US' },
 
     // UK precedes size.
-    {line: 'UK8', size: '8', width: null, intl: 'UK'},
-    {line: 'UK 8', size: '8', width: null, intl: 'UK'},
-    {line: 'UK8D', size: '8', width: 'D', intl: 'UK'},
-    {line: 'UK 8D', size: '8', width: 'D', intl: 'UK'},
-    {line: 'UK8.5', size: '8.5', width: null, intl: 'UK'},
-    {line: 'UK 8.5', size: '8.5', width: null, intl: 'UK'},
-    {line: 'UK8.5D', size: '8.5', width: 'D', intl: 'UK'},
-    {line: 'UK 8.5D', size: '8.5', width: 'D', intl: 'UK'},
+    { line: 'UK8', size: '8', width: null, intl: 'UK' },
+    { line: 'UK 8', size: '8', width: null, intl: 'UK' },
+    { line: 'UK8D', size: '8', width: 'D', intl: 'UK' },
+    { line: 'UK 8D', size: '8', width: 'D', intl: 'UK' },
+    { line: 'UK8.5', size: '8.5', width: null, intl: 'UK' },
+    { line: 'UK 8.5', size: '8.5', width: null, intl: 'UK' },
+    { line: 'UK8.5D', size: '8.5', width: 'D', intl: 'UK' },
+    { line: 'UK 8.5D', size: '8.5', width: 'D', intl: 'UK' },
 
     // EU precedes size.
-    {line: 'EU44', size: '44', width: null, intl: 'EU'},
-    {line: 'EU 44', size: '44', width: null, intl: 'EU'},
-    {line: 'EU44.0', size: '44.0', width: null, intl: 'EU'},
-    {line: 'EU 44.0', size: '44.0', width: null, intl: 'EU'},
+    { line: 'EU44', size: '44', width: null, intl: 'EU' },
+    { line: 'EU 44', size: '44', width: null, intl: 'EU' },
+    { line: 'EU44.0', size: '44.0', width: null, intl: 'EU' },
+    { line: 'EU 44.0', size: '44.0', width: null, intl: 'EU' },
 
     // EUR precedes size.
-    {line: 'EUR44', size: '44', width: null, intl: 'EU'},
-    {line: 'EUR 44', size: '44', width: null, intl: 'EU'},
-    {line: 'EUR44.0', size: '44.0', width: null, intl: 'EU'},
-    {line: 'EUR 44.0', size: '44.0', width: null, intl: 'EU'},
+    { line: 'EUR44', size: '44', width: null, intl: 'EU' },
+    { line: 'EUR 44', size: '44', width: null, intl: 'EU' },
+    { line: 'EUR44.0', size: '44.0', width: null, intl: 'EU' },
+    { line: 'EUR 44.0', size: '44.0', width: null, intl: 'EU' },
   ];
 
-  input_expectations_precedes.forEach((obj) => {
+  inputExpectationsPrecedes.forEach(obj => {
     it(`Should extract ordered data from '${obj.line}'`, () => {
       const expected = {
         size: obj.size,
         width: obj.width,
         intl: obj.intl,
       };
-      const out = match.precedes_match(obj.line);
+      const out = match.precedesMatch(obj.line);
       assert.deepStrictEqual(out, expected);
     });
   });
 
-  input_expectations_precedes.forEach((obj) => {
+  inputExpectationsPrecedes.forEach(obj => {
     it(`Should extract ordered data when addtl notes are present from '${obj.line}'`, () => {
       const expected = {
         size: obj.size,
         width: obj.width,
         intl: obj.intl,
       };
-      const out = match.precedes_match(obj.line + notes);
+      const out = match.precedesMatch(obj.line + notes);
       assert.deepStrictEqual(out, expected);
     });
   });
 
   // Expected fails.
   it.skip('Should extract ordered data when not space delimited', () => { 
-    const xfail_cases = [
-      {line: '7.5EUK', size: '7.5', width: 'E', intl: 'UK'},
-      {line: '7.5EUS', size: '7.5', width: 'E', intl: 'US'},
+    const xfailCases = [
+      { line: '7.5EUK', size: '7.5', width: 'E', intl: 'UK' },
+      { line: '7.5EUS', size: '7.5', width: 'E', intl: 'US' },
     ];
 
-    xfail_cases.forEach((obj) => {
+    xfailCases.forEach(obj => {
       const expected = {
         size: obj.size,
         width: obj.width,
         intl: obj.intl,
       };
-      const out = match.post_match(obj.line);
+      const out = match.postMatch(obj.line);
       assert.deepStrictEqual(out, expected);
     });
   });
