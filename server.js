afd198cb599eae4ca89a05753adf3b5cde667d2a
David Law <davidsamuellaw@gmail.com>
2018-07-07 16:30:58 -0700

    Use new datastore class in server
    Switch to camelCase
    Move template loading to routes
    Populate the datastore within server intialization block

--- server.js
+++ server.js
@@ -1,42 +1,47 @@
 const express = require('express');
 const path = require('path');
-const {SizingData} = require('./src/container.js');
+const { flatMap, memoize } = require('lodash');
+const io = require('./src/io.js');
+const { DataStore, loadData } = require('./src/store.js');
+
 const app = express();
-
-
 const constants = {
   port: 3030,
-  data_dir: './src/data',
+  dataDir: './src/data',
   filenames: [
       'last_sizing_thread_2017.json',
       'last_sizing_thread_2018.json',
   ],
 };
 
-const data_filenames = constants.filenames.map(fname => {
-  return path.join(constants.data_dir, fname);
-});
-const sd = SizingData.from_files(data_filenames);
+const dataFilenames = constants.filenames
+  .map(fname => path.join(constants.dataDir, fname));
+const dataStore = new DataStore(flatMap(dataFilenames, loadData));
+
+const getTemplate = memoize(io.getTemplate);
 
 
 app.get('/', (req, res) => {
-  let html = sd.render_sizes('./templates/index.html');
+  const renderer = getTemplate('./templates/index.html');
+  const html = renderer(dataStore);
   res.send(html);
 });
 
 app.get('/sizing', (req, res) => {
-  res.json(sd.get());
+  const data = dataStore.get();
+  res.json(data);
 });
 
 app.get('/sizes/:size', (req, res) => {
+  const data = dataStore.getSize(req.params.size);
   res.format({
     'default': () => {
-      let html = sd.render_data('./templates/table_rows.html', req.params.size);
+      const renderer = getTemplate('./templates/table_rows.html');
+      const html = renderer({ data });
       res.send(html);
     },
     'application/json': () => {
-      let response_data = sd.get_size(req.params.size);
-      res.send(response_data);
+      res.send(data);
     },
   });
 });
